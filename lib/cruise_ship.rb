4b53d207a317b8427033e8385172ceddb8088d86
Jose Mendoza <pball626@gmail.com>
2021-02-10 02:29:26 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -13,7 +13,6 @@ def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
-      binding.pry
     end
   end
   winner
